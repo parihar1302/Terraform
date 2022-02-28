4aa3af8dde60843df056ebb2cf3e21d9f6c80a54
parihar1302 <45783975+parihar1302@users.noreply.github.com>
2022-02-28 11:46:30 +0530

    Added multi block

---
+++ multiblock.tf
@@ -0,0 +1,13 @@
+//multi block file
+
+output first {                   //lable must be different  
+    value = "this is bharat"     //value could be the same
+}
+
+output second {
+    value = "this is hindustan"
+}
+
+output thired {
+    value = "this is india"
+}
