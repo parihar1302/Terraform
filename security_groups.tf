770a879be5f8de3b7a98f860ff7f560a760e2ce4
parihar1302 <45783975+parihar1302@users.noreply.github.com>
2022-03-04 17:05:04 +0530

    added security group module

---
+++ security_groups.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "main" {
+  name = "terraformSGNTS"
+//outgoing traffic
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+//incoming traffic
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+// below command needs to be added as module in resource "aws_instance" "terraformec2" in main.tf file 
+//security_groups = ["terraformSGNTS"]
+//=======================================================================================================
