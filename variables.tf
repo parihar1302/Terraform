68ad14d1716c9d3092d6b66813f7d12af4fdd073
parihar1302 <45783975+parihar1302@users.noreply.github.com>
2022-10-13 23:23:16 +0530

    modules updated

--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 #Variable.tf = will store variable which you can call from main.tf. It provides you flexibility to define the value & modify it base on requirement.
 
 #vi variablle.tf         
+
+variable "region" {
+  default = "us-east-1"
+}
 #variable is for instance type
 variable "instance_type" {
    description = "Instance type t2.micro"    ## description
    type        = string                      ## number, string, boolean, float, list, map
    default     = "t2.micro"                  ##variable default value which you can modify
+}
+
+variable "ami_id" {
+  default = "ami-035b3c7efe6d061d5"
+}
+
+variable "tag" {
+  default = "t2.micro"
 }
 
 # now variable is for s3
@@ -23,7 +35,7 @@ variable "s3_bucket_names" {
 
 #vi main.tf
 provider "aws" {                                            
-   region     = "us-east-2"
+   region     = "${var.region}"
    access_key = "AKIA3KP2SFFPOZHK5TFEABHAI"     
    secret_key = "H/oQe2a5CpFt6vGokkAyz5HP3BqYDEdsVfRCaSSk130A2"
    
@@ -31,10 +43,9 @@ provider "aws" {
 
 resource "aws_instance" "terraformec2" {
 
-    ami = "ami-0fb653ca2d3203ac1"  
-    instance_type = var.instance_type         ##calling variable here
+    ami_id                     = "${var.ami_id}"
+    instance_type              = "${var.instance_type}"
+    tag                        = "${var.tag}"
     key_name= "aws_key"
-  	tags = {
-       Name = "terraformNTS"
     }
 }
